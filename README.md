00eb620c11374e32e82f6bca297a72b35be50198
DrMaxis <nathankantwi@gmail.com>
2022-01-17 23:54:03 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-deployment-clearer-m5vx5e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-deployment-clearer-m5vx5e)
---
+++ index.js
@@ -0,0 +1,66 @@
+import "./style.css";
+
+// RECOMMENDED: Disconnect HEROKU from Github before doing this (though not strictly necessary, I think).
+//See https://stackoverflow.com/a/61272173/6569950 for more info.
+
+// PARAMETERS
+const TOKEN = "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"; // MUST BE `repo_deployments` authorized
+const REPO = "unwantedapp"; // e.g. "monorepo"
+const USER_OR_ORG = "studio-unwanted"; // e.g. "your-name"
+
+// GLOBAL VARS
+const URL = `https://api.github.com/repos/${USER_OR_ORG}/${REPO}/deployments`;
+const AUTH_HEADER = `token ${TOKEN}`;
+
+// UTILITY FUNCTIONS
+const getAllDeployments = () =>
+  fetch(`${URL}`, { headers: { authorization: AUTH_HEADER } }).then(val => val.json());
+
+const makeDeploymentInactive = id =>
+  fetch(`${URL}/${id}/statuses`, {
+    method: "POST",
+    body: JSON.stringify({ state: "inactive" }),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.ant-man-preview+json",
+      authorization: AUTH_HEADER
+    }
+  }).then(() => id);
+
+const deleteDeployment = id =>
+  fetch(`${URL}/${id}`, {
+    method: "DELETE",
+    headers: { authorization: AUTH_HEADER }
+  }).then(() => id);
+
+// MAIN
+getAllDeployments()
+  .catch(console.error)
+  .then(res => {
+    console.log(`${res.length} deployments found`);
+    return res;
+  })
+  .then(val => val.map(({ id }) => id))
+  .then(ids => Promise.all(ids.map(id => makeDeploymentInactive(id))))
+  .then(res => {
+    console.log(`${res.length} deployments marked as "inactive"`);
+    return res;
+  })
+  .then(ids => Promise.all(ids.map(id => deleteDeployment(id))))
+  .then(res => {
+    console.log(`${res.length} deployments deleted`);
+    return res;
+  })
+  .then(finalResult => {
+    const appDiv = document.getElementById("app");
+    appDiv.innerHTML = `
+<h1>CLEANUP RESULT</h1>
+<br>
+Removed Deployments: ${finalResult.length}
+<br>
+<br>Ids:<br>
+${JSON.stringify(finalResult)}
+<br><br><br><br><br><br>
+  <p>(Open up the console)</p>
+`;
+  });
